ea5d9e6ffa86742bf638a5a0d4996929a81db2a6
Gan Zejun <ganzejun@zhongan.io>
2019-06-21 16:18:38 +0800

    Added type to log event

--- processors/logevent.go
+++ processors/logevent.go
@@ -2,6 +2,7 @@ package processors
 
 type LogEvent struct {
 	Identity  string `json:"identity"`
+	Type      string `json:"type"`
 	Ip        string `json:"ip"`
 	Timestamp int    `json:"timestamp"`
 	Data      string `json:"data"`
