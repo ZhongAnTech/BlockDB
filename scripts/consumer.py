ec263615154d129925470bf9ab1e9fa9bb115cfa
Gan Zejun <ganzejun@zhongan.io>
2019-07-16 13:58:39 +0800

    deployment fix

---
+++ scripts/consumer.py
@@ -0,0 +1,10 @@
+from kafka import KafkaConsumer
+
+if __name__ == '__main__':
+    consumer = KafkaConsumer('anlink', bootstrap_servers=['47.100.222.11:30000'])
+    for message in consumer:
+        # message value and key are raw bytes -- decode if necessary!
+        # e.g., for unicode: `message.value.decode('utf-8')`
+        print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                              message.offset, message.key,
+                                              message.value))
---
+++ scripts/kafka_simple_producer.py
@@ -0,0 +1,36 @@
+import datetime
+import json
+from kafka import KafkaConsumer, KafkaProducer
+
+d = {
+    "private_data": "Your own data here",
+    "my_array": ["It", "supports", "array"],
+    "my_inner_object": {
+        "supported": True,
+        "when": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    }
+
+}
+e = {
+    "identity": "user_id_234823",
+    "type": "system",
+    "ip": "222.333.22.33",
+    "primary_key": "unique_id_2852",
+    "timestamp": int(datetime.datetime.now().timestamp()) * 1000,
+    "data": d,
+    "before": None,
+    "after": None,
+
+}
+
+if __name__ == '__main__':
+    producer = KafkaProducer(bootstrap_servers=['47.100.222.11:30000'])
+
+    for i in range(1):
+        ss = json.dumps(e)
+        # ss += '\0'
+        # producer.send('tech-tech-anlink-web-gateway-201907101551', bytes(ss, 'utf-8'))
+        producer.send('anlink', bytes(ss, 'utf-8'))
+        producer.flush()
+        print(ss)
+
