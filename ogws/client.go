43e65edc598bb684c7d7478f30d4261579ac6aad
Gan Zejun <ganzejun@zhongan.io>
2019-08-07 11:12:37 +0800

    added some debug info

--- ogws/client.go
+++ ogws/client.go
@@ -111,7 +111,7 @@ func (o *OGWSClient) handleMessage(bytes []byte) (result OGMessageList, err erro
 			logrus.WithError(err).Warn("failed to write ledger.")
 			continue
 		}
-		logrus.WithField("event timestamp", auditEvent.Data.Height).Debug("wrote audit event")
+		logrus.WithField("event timestamp", auditEvent.Height).Debug("wrote audit event")
 	}
 	return
 }
