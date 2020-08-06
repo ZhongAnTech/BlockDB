e0bab404f41ea7670468fefb4f0ebf05b8b7f96f
Zhang Zhanpeng <zhangregister@outlook.com>
2020-08-06 22:15:19 +0800

    2020-08-06-1

--- poc/rpc_test/new_transaction/new_transaction.go
+++ poc/rpc_test/new_transaction/new_transaction.go
@@ -5,6 +5,11 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"sync"
+
 	"github.com/annchain/BlockDB/poc/rpc_test/og"
 	"github.com/annchain/OG/common"
 	"github.com/annchain/OG/common/crypto"
@@ -12,59 +17,78 @@ import (
 	"github.com/annchain/OG/rpc"
 	"github.com/annchain/OG/types"
 	"github.com/annchain/OG/types/tx_types"
-	"io/ioutil"
-	"net/http"
 )
 
-// 2个账户的公钥、私钥
 const (
+	// 2个账户的私钥
 	privKey0 = "0x01a03846356c336844979e7972916b9b045071a57e532457576dd65e89952d9154"
-	pubKey0  = "0x010414018666329d97625809f0b8daf7ea052e7dac810dbfcf67ac31d9543bd2937123a984a681b4102c2fce907afc52df4edffb602134fc52e58f8c7b8e04d25dc8"
 	privKey1 = "0x01e32e537bd309f0c97ccec33c1d016c3b7e3561760ad574364fdf7ef07fc876ac"
-	pubKey1  = "0x010442bd7bd58f46c2c3b5d69d20227c30a8dd937306122722b170c935cbb0319e170cd4af39f3f4e51abbe5910a24925153567cda2961768e99951cfbecd4e489af"
 
-	url                  = "http://localhost:8000" /* 远程RPC调试 */
-	defaultValue         = 0                       /* 缺省转账额 */
-	defaultData          = ""                      /* 缺省数据 */
-	defaultCryptoType    = "secp256k1"             /* 加密类型 */
-	defaultTokenID       = int64(0)                /* 缺省安全令牌 */
-	transactionRPCMethod = "new_transaction"       /* 交易RPC方法 */
-	nonceMethod          = "query_nonce"
+	url                  = "http://47.100.122.212:30022" /* 远程RPC调试 */
+	defaultValue         = "0"                           /* 缺省转账额 */
+	defaultData          = "="                           /* 缺省交易数据 */
+	defaultCryptoType    = "secp256k1"                   /* 缺省加密类型 */
+	defaultTokenID       = 0                             /* 缺省安全令牌 */
+	transactionRPCMethod = "new_transaction"             /* 新建交易RPC方法 */
+	nonceRPCMethod       = "query_nonce"                 /* 查询nonceRPC方法 */
+
+	countOfTXPerCoroutine = 10 /* 每个协程发送的交易数目 */
+	countOfCoroutine      = 3  /* 协程数目 */
 )
 
-//// Account 账户字段
-//type Account struct {
-//	PublicKey  string /* 公钥，十六进制字符串类型 */
-//	PrivateKey string /* 私钥，十六进制字符串类型 */
-//	Nonce      int64  /* 账户发出交易次数 */
-//	Addr       string /* 地址，十六进制字符串类型 */
-//}
+var (
+	wg sync.WaitGroup
+)
 
-// TX 交易字段
-type TX struct {
-	Nonce         int64  `json:"nonce"`       /* 支出账户发出交易次数 */
-	FromAddr      string `json:"from"`        /* 支出账户地址 */
-	ToAddr        string `json:"to"`          /* 收入账户地址 */
-	Value         string `json:"value"`       /* 交易额 */
-	Data          string `json:"data"`        /* 交易规则 */
-	CryptoType    string `json:"crypto_type"` /* 加密类型 */
-	Signature     string `json:"signature"`   /* 签名 */
-	FromPublicKey string `json:"pubkey"`      /* 支出账户公钥，十六进制字符串类型 */
-	TokenID       int64  `json:"token_id"`    /* 安全令牌 */
+// NonceResponse 更新nonce的回复消息
+type NonceResponse struct {
+	Nonce uint64 `json:"data"`
+	Err   string `json:"err"`
 }
 
-func newTX(from *og.SampleAccount, to *og.SampleAccount) *rpc.NewTxRequest {
-	signer := crypto.NewSigner(from.PrivateKey.Type)
+// TransactionResponse 交易回复消息
+type TransactionResponse struct {
+	Data string `json:"data"`
+	Err  string `json:"err"`
+}
 
-	// TODO 签名，query nonce
-	// consume nonce
-	nonce, err := from.ConsumeNonce()
+// UpdateNonce 更新nonce，跟链同步
+func UpdateNonce(account *og.SampleAccount) uint64 {
+	postURL := url + "/" + nonceRPCMethod
+	req, err := http.NewRequest("GET", postURL+"?address="+account.Address.String(), nil)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	nr := &NonceResponse{}
+	err = json.Unmarshal(respBody, nr)
+	if err != nil {
+		print(err)
+	}
+	if nr.Err != "" {
+		print(nr.Err)
+	}
+	account.SetNonce(nr.Nonce)
+	return nr.Nonce
+}
 
-	// This is an OG Tx to show what signature target is.
-	// No need to send all stuff to OG.
+// NewTX 新建交易
+func NewTX(from *og.SampleAccount, to *og.SampleAccount) *rpc.NewTxRequest {
+	// Nonce
+	nonce, err := from.ConsumeNonce() /* 消费nonce */
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Signature
+	signer := crypto.NewSigner(from.PrivateKey.Type)
 	tx := tx_types.Tx{
 		TxBase: types.TxBase{
 			Type:         0,
@@ -84,33 +108,28 @@ func newTX(from *og.SampleAccount, to *og.SampleAccount) *rpc.NewTxRequest {
 		TokenId: 0,
 		Data:    nil,
 	}
-
-	fmt.Println(hex.EncodeToString(tx.SignatureTargets()))
-	fmt.Println(hex.EncodeToString(from.PublicKey.Bytes))
-	sig := signer.Sign(from.PrivateKey, tx.SignatureTargets())
-
-	// This is an OG rpc Tx to show what should be sent to OG.
+	sig := signer.Sign(from.PrivateKey, tx.SignatureTargets()) /* 从私钥生成签名 */
 	return &rpc.NewTxRequest{
 		Nonce:      nonce,
 		From:       from.Address.String(),
 		To:         to.Address.String(),
-		Value:      "0",
-		Data:       "=",
+		Value:      defaultValue,
+		Data:       defaultData,
 		CryptoType: defaultCryptoType,
 		Signature:  hex.EncodeToString(sig.Bytes),
 		Pubkey:     hex.EncodeToString(from.PublicKey.Bytes),
-		TokenId:    0,
+		TokenId:    defaultTokenID,
 	}
 }
 
-func transaction(from *og.SampleAccount, to *og.SampleAccount) string {
-	postURL := url + "/" + transactionRPCMethod /* 新交易URL */
-	post, err := json.Marshal(newTX(from, to))
+// Transaction 交易
+func Transaction(from *og.SampleAccount, to *og.SampleAccount) []byte {
+	postURL := url + "/" + transactionRPCMethod
+	post, err := json.Marshal(NewTX(from, to))
 	if err != nil {
 		fmt.Println(err)
 	}
 	postBuffer := bytes.NewBuffer(post)
-	fmt.Println(postBuffer.String())
 	req, err := http.NewRequest("POST", postURL, postBuffer)
 	if err != nil {
 		fmt.Println(err)
@@ -124,48 +143,35 @@ func transaction(from *og.SampleAccount, to *og.SampleAccount) string {
 	if err != nil {
 		fmt.Println(err)
 	}
-	return string(respBody)
+	return respBody
 }
 
-type NonceResponse struct {
-	Nonce uint64 `json:"data"`
-	Err   string `json:"err"`
-}
-
-func updateNonce(account *og.SampleAccount) uint64 {
-	postURL := url + "/" + nonceMethod /* 新交易URL */
-
-	req, err := http.NewRequest("GET", postURL+"?address="+account.Address.String(), nil)
-	if err != nil {
-		fmt.Println(err)
+// 每个协程的任务
+func handle(from *og.SampleAccount, to *og.SampleAccount) {
+	defer wg.Done()
+	for i := 0; i < countOfTXPerCoroutine; i++ {
+		tr := &TransactionResponse{}
+		err := json.Unmarshal(Transaction(from, to), tr)
+		if err != nil {
+			print(err)
+		}
+		if tr.Err != "" {
+			print(tr.Err)
+			continue
+		}
+		if tr.Data != "" {
+			fmt.Println(strings.TrimPrefix(tr.Data, "0x"))
+		}
 	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer resp.Body.Close()
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(string(respBody))
-	nr := &NonceResponse{}
-	err = json.Unmarshal(respBody, nr)
-	if err != nil {
-		print(err)
-	}
-	if nr.Err != "" {
-		print(nr.Err)
-	}
-	account.SetNonce(nr.Nonce)
-	return nr.Nonce
 }
 
 func main() {
 	account0 := og.NewAccount(privKey0)
 	account1 := og.NewAccount(privKey1)
-
-	updateNonce(account0)
-	// update nonce first.
-	fmt.Println("result " + transaction(account0, account1))
+	UpdateNonce(account0)
+	for i := 0; i < countOfCoroutine; i++ {
+		wg.Add(1)
+		go handle(account0, account1)
+	}
+	wg.Wait()
 }
--- poc/rpc_test/og/account.go
+++ poc/rpc_test/og/account.go
@@ -2,58 +2,49 @@ package og
 
 import (
 	"fmt"
+	"sync"
+
 	"github.com/annchain/OG/common"
 	"github.com/annchain/OG/common/crypto"
 	"go.uber.org/atomic"
-	"sync"
 )
 
+// SampleAccount 账户
 type SampleAccount struct {
-	Id          int
 	PrivateKey  crypto.PrivateKey
 	PublicKey   crypto.PublicKey
 	Address     common.Address
 	nonce       atomic.Uint64
 	nonceInited bool
 	mu          sync.RWMutex
-	InitBalance uint64
 }
 
+// NewAccount 新建账户
 func NewAccount(privateKeyHex string) *SampleAccount {
 	s := &SampleAccount{}
-	pv, err := crypto.PrivateKeyFromString(privateKeyHex)
+	pr, err := crypto.PrivateKeyFromString(privateKeyHex)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	signer := crypto.NewSigner(pv.Type)
-	s.PrivateKey = pv
-	s.PublicKey = signer.PubKey(pv)
+	signer := crypto.NewSigner(pr.Type)
+	s.PrivateKey = pr
+	s.PublicKey = signer.PubKey(pr)
 	s.Address = signer.Address(s.PublicKey)
-	//logrus.WithField("add", s.Address.String()).WithField("priv", privateKeyHex).Trace("Sample Account")
-
 	return s
 }
 
+// ConsumeNonce 消费nonce
 func (s *SampleAccount) ConsumeNonce() (uint64, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-
 	if !s.nonceInited {
 		return 0, fmt.Errorf("nonce is not initialized. Query first")
 	}
-	s.nonce.Inc()
+	s.nonce.Inc() /* nonce++，包装并返回 */
 	return s.nonce.Load(), nil
 }
 
-func (s *SampleAccount) GetNonce() (uint64, error) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	if !s.nonceInited {
-		return 0, fmt.Errorf("nonce is not initialized. Query first")
-	}
-	return s.nonce.Load(), nil
-}
-
+// SetNonce 人为初始化nonce
 func (s *SampleAccount) SetNonce(lastUsedNonce uint64) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
